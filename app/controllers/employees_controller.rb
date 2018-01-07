4465cdaca2aebe67a75f2bd46202063b0d7e8d17
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2018-01-07 09:12:40 +0100

    Create form and validation for add employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -28,8 +28,8 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
-        format.json { render :show, status: :created, location: @employee }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully created.' }
+        format.json { render :index, status: :created, location: @employee }
       else
         format.html { render :new }
         format.json { render json: @employee.errors, status: :unprocessable_entity }
@@ -42,8 +42,8 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully updated.' }
+        format.json { render :index, status: :ok, location: @employee }
       else
         format.html { render :edit }
         format.json { render json: @employee.errors, status: :unprocessable_entity }
